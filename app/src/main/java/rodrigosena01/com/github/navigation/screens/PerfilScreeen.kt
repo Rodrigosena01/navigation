bae158c138b4432913f9165cf530d290b07e3e91
rodrigosena01 <rodrigo11sena@gmail.com>
2026-03-20 16:25:23 -0300

    first commit

---
+++ app/src/main/java/rodrigosena01/com/github/navigation/screens/PerfilScreeen.kt
@@ -0,0 +1,31 @@
+package rodrigosena01.com.github.navigation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun PerfilScreen(modifier: Modifier = Modifier, navController: NavController) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(Color(0xFFFA9A9A9))
+        .padding(32.dp)
+    ) {
+        Text(
+            text = "PEDIDOS",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+
+    }
+}
